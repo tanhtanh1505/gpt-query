4ccf4c5dcf552dc16c2f3f2bcfa0d1e65b96420e
tanhtanh1505 <tanhtanh1505@gmail.com>
2023-05-28 03:43:09 +0700

    fix: Scripts up to server

--- server/scripts/move_to_server.sh
+++ server/scripts/move_to_server.sh
@@ -1,2 +1,2 @@
 cd ../../roofy
-scp -i roofy-server.pem ../gpt-query/server/gpt-query-prod.tar ubuntu@ec2-13-212-216-240.ap-southeast-1.compute.amazonaws.com:~/gpt-query
+scp -i llele-server.pem ../gpt-query/server/gpt-query-prod.tar ubuntu@ec2-54-169-39-45.ap-southeast-1.compute.amazonaws.com:~/gpt-query
